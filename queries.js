3e12d9da64b635faf74cace0b576e82558340d08
iTwicKz <twickes32@ufl.edu>
2016-01-20 13:46:43 -0500

    Added findAll and removeCABL

--- queries.js
+++ queries.js
@@ -16,12 +16,12 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console.
    */
-
-
   Listing.findOne({ 'name': 'Library West' }, function (err, listing) {
   if (err) return handleError(err);
     console.log(listing) // Space Ghost is a talk show host.
   })
+
+
 
 };
 var removeCable = function() {
@@ -30,20 +30,51 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console.
    */
+   Listing.findOne({ 'code': 'CABL' }, function (err, listing) {
+   if (err) return handleError(err);
+     console.log(listing) // Space Ghost is a talk show host.
+
+   })
+   Listing.remove({ 'code': 'CABL' }, function (err) {
+   if (err) return handleError(err);
+   })
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
     log the updated document to the console.
    */
+   Listing.collection.update(
+     { "code": "PHL"},
+     {
+       "address": "AHHHHH LOOK AT THTA IT CHANGED"
+     }
+   )
+
+   Listing.findOne({ 'code': 'PHL' }, function (err, listing) {
+   if (err) return handleError(err);
+     console.log(listing) // Space Ghost is a talk show host.
+   })
+
+  //  MyModel.findOneAndUpdate({ "code": "PHL"}, listing.newData, {upsert:true}, function(err, listing){
+  //   if (err) return res.send(500, { error: err });
+  //   return res.send("succesfully saved");
+  //   });
+   //
+   //
+
 };
 var retrieveAllListings = function() {
   /*
     Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find(function (err, listing) {
+     if (err) return console.error(err);
+     console.log(listing);
+   })
 };
-
-findLibraryWest();
-removeCable();
+//
+// findLibraryWest();
+// removeCable();
 updatePhelpsMemorial();
 retrieveAllListings();
